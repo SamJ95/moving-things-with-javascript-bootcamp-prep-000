ec8d8a0b85b16c9f6489ccda642e15763ba9f693
Samir Jasarat <samirjasarat@gmail.com>
2019-08-14 17:26:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = "I don't do much."
 document.addEventListener('keydown',
 function(e) {
   if (e.which === 37) {
-    var leftNumbers = 
+    var leftNumbers =
     dodger.style.left.replace('px', '')
     var left = parseInt (leftNumbers, 10)
 
